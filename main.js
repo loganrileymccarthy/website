6e7100a984a7cdaa0b7f06f04a93f535f8712e7e
loganrileymccarthy <loganrileymccarthy@gmail.com>
2025-04-19 22:10:33 -0700

    header footer script

---
+++ main.js
@@ -0,0 +1,34 @@
+class MyHeader extends HTMLElement {
+	connectedCallback() {
+	this.innerHTML = '
+		<header>
+        		<h1>
+				<a href="../index.html" style="color:white;text-decoration:none;">&#x2694 LRM &#x2694</a>
+			</h1>
+			<h2>
+				<a href="../music.html">music</a> // 
+				<a href="../artwork.html">artwork</a> // 
+				<a href="../slugshots.html">slugshots</a> // 
+				<a href="../for-sale.html">for sale</a> // 
+				<a href="../workout.html">workout</a> // 
+				<a href="../running.html">running PRs</a>
+        		</h2>
+    		</header>
+	'
+	}
+}
+
+customElements.define('my-header', MyHeader)
+
+class MyFooter extends HTMLElement {
+	connectedCallback() {
+	this.innerHTML = '
+		<footer>
+			2025
+		</footer>
+	'
+	}
+}
+
+customElements.define('my-footer', MyFooter)
+
