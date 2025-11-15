1d210b0a5cd794952d93ab2c7edce2fa6696713b
Logan <148300657+loganrileymccarthy@users.noreply.github.com>
2025-11-15 14:10:15 -0800

    Update main.js

--- main.js
+++ main.js
@@ -2,20 +2,19 @@ class MyHeader extends HTMLElement {
 	connectedCallback() {
 	this.innerHTML = `
 		<header>
-        	<h1>
-				<a href="../index.html" style="{color:white;text-decoration:none;}">&#x2694 LRM &#x2694</a>
+      		<h1>
+				<a href="../index.html" class="homeButton">&#x2694 LRM &#x2694</a>
 			</h1>
 			<h2>
    				<br>
 				<a href="../music.html">music</a> // 
 				<a href="../artwork.html">artwork</a> // 
 				<a href="../tools.html">tools</a> // 
-    				<a href="../slugshots.html">slugshots</a> // 
+    			<a href="../slugshots.html">slugshots</a> // 
 				<a href="../workout.html">workout</a> // 
 				<a href="../running.html">running PRs</a>
-				
-        		</h2>
-    		</header>
+    		</h2>
+    	</header>
 	`
 	}
 }
